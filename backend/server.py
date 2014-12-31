9adbb043d8a48f775e087abd74a49f223faf02c1
Harlan Haskins <harlan@harlanhaskins.com>
2014-12-30 22:01:18 -0500

    Moved backend to backend folder

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+import argparse
 import cardkov
 
 app = Flask(__name__)
+cors = CORS(app)
 
 base = "/api"
 
@@ -9,7 +12,7 @@ max_quotes = 100
 
 @app.route(base + "/quotes", methods=["GET"])
 def quote():
-    count_string = request.args.get("count", 1)
+    count_string = request.args.get("count", "1")
     count = int(count_string) if count_string.isdigit() else 1
     count = max(1, min(count, 100))
     quotes = []
@@ -17,4 +20,9 @@ def quote():
         quotes.append(cardkov.new_string())
     return jsonify(quotes=quotes)
 
-app.run(host="0.0.0.0", port=5585, debug=True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--test", help="Runs flask in debug mode.",
+                          action="store_true")
+    args = parser.parse_args()
+    app.run(host="0.0.0.0", port=5585, debug=args.test)
