cc319aea0efca47c88b19363722ce9d829adae86
Harlan Haskins <harlan@harlanhaskins.com>
2015-09-03 20:43:40 -0400

    Updated whole site to be initially generated server-side

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, Response, json, make_response
+from flask import Flask, jsonify, request, Response, json, make_response, render_template
 from flask_cors import CORS
 import argparse
 from homestarkov import Homestarkov
@@ -12,11 +12,19 @@ _characters = {
     "cardgage": Homestarkov("cardgage", "Senor Cardgage",
         "Dump Tell No Mandy!"),
     "homsar": Homestarkov("homsar", "Homsar", "Legitimate Business!"),
-    "hackernews": Homestarkov("hackernews", "Hacker News",
-        "Presented by Y-combinator.")
+    "hackernews": Homestarkov("hackernews", "Hacker News", "Presented by Y-Combinator"),
+    "guyfieri": Homestarkov("guyfieri", "Guy Fieri", "Take me to Flavortown")
 }
 
 max_quotes = 100
+
+@app.route('/<name>', methods=["GET"])
+def character_page(name):
+    character = _characters.get(name)
+    if character is None:
+        abort(404)
+    return render_template('quote.html', character=character)
+
 
 @app.route(base + "/characters", methods=["GET"])
 def characters():
@@ -44,4 +52,4 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--test", help="Runs flask in debug mode.",
                           action="store_true")
     args = parser.parse_args()
-    app.run(host="0.0.0.0", port=5586 if args.test else 5585, debug=args.test)
+    app.run(host="0.0.0.0", port=5585, debug=args.test)
---
+++ src/static/js/app.js
@@ -0,0 +1,30 @@
+(function($, console) {
+
+var twitterUrl = "http://twitter.com/share";
+var facebookUrl = "http://www.facebook.com/sharer.php";
+
+$.fn.getQuote = function () {
+    $.ajax({
+        url: apiUrl,
+        method: "GET",
+        success: function(r) {
+            var quote = r.quotes[0];
+            $("#quote").html("\""+quote+"\"")
+            $(this).populateSocialMedia();
+        },
+        error: function(e) {
+            console.error(e);
+            $("#quote").html("ERROR GETTING QUOTE");
+        }
+    })
+}
+
+$.fn.populateSocialMedia = function () {
+    var quote = $('#quote').html();
+    $("#twitter").attr("href", twitterUrl + "?text=" + quote + "&hashtags=QuoteMaker&url=" + frontendUrl);
+    $("#facebook").attr("href", facebookUrl + "?t=" + quote + "&u=" + frontendUrl);
+}
+
+})(jQuery, window.console);
+
+
