1366038c8c9ac5d71215c1070cd35cec13bb26f5
Harlan Haskins <harlan@harlanhaskins.com>
2015-09-04 15:26:51 -0400

    Fixed API

--- QuoteMaker/quote/api.py
+++ QuoteMaker/quote/api.py
@@ -1,9 +1,10 @@
 from .models import *
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, JsonResponse
 
 def quote(request, path):
     character = get_object_or_404(Homestarkov, path=path)
-    count = request.args.get('count', '1')
+    count = request.GET.get('count', '1')
     if not count.isdigit():
         return HttpResponse({'message', 'count must be a number'}, 412)
     count = min(str(count), 100)
